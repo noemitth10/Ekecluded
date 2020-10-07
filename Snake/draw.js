d5c95db7b581b093f7b45ead81f350e0789831ec
Kornel0120 <61868148+Kornel0120@users.noreply.github.com>
2020-10-07 18:14:30 +0200

    Create draw.js
    Snake draw.js created and finished

---
+++ Snake/draw.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+let count = 0;
+var snake;
+(function setup(){
+    snake = new Snake();
+    apple = new Apple();
+    apple.pickLocation();
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        snake.update();
+        apple.draw();
+        snake.draw();
+        if (snake.eat(apple)){
+            apple.pickLocation();
+            count++;
+            document.getElementById('counter').innerHTML = count;
+        }
+    }, 250)
+}());
+window.addEventListener('keydown', ((evt) => {
+    const direction = evt.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+}));
